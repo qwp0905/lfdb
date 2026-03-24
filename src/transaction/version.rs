d6bf38741e10efd6dd0668e30546ec6ea5ef0dff
qwp0905 <qwp0905@gmail.com>
2026-03-24 13:57:57 +0900

    add new state struct as alias of skiplist entry since epoch gc

--- src/transaction/version.rs
+++ src/transaction/version.rs
@@ -1,13 +1,71 @@
 use std::{
   panic::RefUnwindSafe,
-  sync::atomic::{AtomicUsize, Ordering},
+  sync::atomic::{AtomicU8, AtomicUsize, Ordering},
 };
 
-use crossbeam_skiplist::SkipSet;
+use crossbeam_skiplist::{map::Entry, SkipMap, SkipSet};
+
+const STATUS_AVAILABLE: u8 = 0;
+const STATUS_ON_COMMIT: u8 = 1; // Exclusive state prior to commit
+const STATUS_COMMITTED: u8 = 2; // The commit log has been successfully written.
+const STATUS_ABORTED: u8 = 3;
+
+pub struct TxState<'a>(Entry<'a, usize, AtomicU8>);
+impl<'a> TxState<'a> {
+  #[inline(always)]
+  pub fn get_id(&self) -> usize {
+    *self.0.key()
+  }
+  #[inline]
+  pub fn is_available(&self) -> bool {
+    self.0.value().load(Ordering::Acquire) == STATUS_AVAILABLE
+  }
+  #[inline]
+  pub fn try_abort(&self) -> bool {
+    self
+      .0
+      .value()
+      .compare_exchange(
+        STATUS_AVAILABLE,
+        STATUS_ABORTED,
+        Ordering::Release,
+        Ordering::Acquire,
+      )
+      .is_ok()
+  }
+
+  #[inline]
+  pub fn try_commit(&self) -> bool {
+    self
+      .0
+      .value()
+      .compare_exchange(
+        STATUS_AVAILABLE,
+        STATUS_ON_COMMIT,
+        Ordering::Release,
+        Ordering::Acquire,
+      )
+      .is_ok()
+  }
+
+  #[inline(always)]
+  pub fn deactive(&self) {
+    self.0.remove();
+  }
+
+  #[inline]
+  pub fn complete_commit(&self) {
+    self.0.value().store(STATUS_COMMITTED, Ordering::Release)
+  }
+  #[inline]
+  pub fn make_available(&self) {
+    self.0.value().store(STATUS_AVAILABLE, Ordering::Release)
+  }
+}
 
 pub struct VersionVisibility {
   aborted: SkipSet<usize>,
-  active: SkipSet<usize>,
+  active: SkipMap<usize, AtomicU8>,
   last_tx_id: AtomicUsize,
 }
 impl VersionVisibility {
@@ -38,27 +96,27 @@ impl VersionVisibility {
     self
       .active
       .front()
-      .map(|v| *v.value())
+      .map(|v| *v.key())
       .unwrap_or_else(|| self.current_version())
   }
-  pub fn deactive(&self, tx_id: &usize) {
-    self.active.remove(tx_id);
-  }
   pub fn is_visible(&self, tx_id: &usize) -> bool {
-    !self.aborted.contains(tx_id) && !self.active.contains(tx_id)
+    !self.aborted.contains(tx_id) && !self.active.contains_key(tx_id)
   }
   pub fn is_active(&self, tx_id: &usize) -> bool {
-    self.active.contains(tx_id)
+    self.active.contains_key(tx_id)
   }
-  pub fn move_to_abort(&self, tx_id: usize) {
-    self.active.remove(self.aborted.insert(tx_id).value());
+  pub fn set_abort(&self, tx_id: usize) {
+    self.aborted.insert(tx_id);
   }
-  pub fn new_transaction(&self) -> usize {
+  pub fn new_transaction(&self) -> TxState<'_> {
     let tx_id = self.last_tx_id.fetch_add(1, Ordering::Release);
-    *self.active.insert(tx_id).value()
+    TxState(self.active.insert(tx_id, AtomicU8::new(STATUS_AVAILABLE)))
   }
   pub fn current_version(&self) -> usize {
     self.last_tx_id.load(Ordering::Acquire)
+  }
+  pub fn get_active_state(&self, tx_id: usize) -> Option<TxState<'_>> {
+    self.active.get(&tx_id).map(TxState)
   }
 }
 impl RefUnwindSafe for VersionVisibility {}
