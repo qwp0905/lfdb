56d6aded2022395bdcc0c944de35fd5b4599009d
qwp0905 <qwp0905@gmail.com>
2026-04-09 22:50:52 +0900

    add pinned handle to track pin

--- src/table/handle.rs
+++ src/table/handle.rs
@@ -1,6 +1,9 @@
-use std::sync::{
-  atomic::{AtomicUsize, Ordering},
-  Arc,
+use std::{
+  ops::Deref,
+  sync::{
+    atomic::{AtomicBool, AtomicUsize, Ordering},
+    Arc,
+  },
 };
 
 use super::TableMetadata;
@@ -15,6 +18,7 @@ pub struct TableHandle {
   disk: DiskController<PAGE_SIZE>,
   free_list: FreeList,
   pin: AtomicUsize,
+  closed: AtomicBool,
 }
 impl TableHandle {
   pub fn open(
@@ -28,14 +32,19 @@ impl TableHandle {
       disk,
       free_list: FreeList::new(),
       pin: AtomicUsize::new(0),
+      closed: AtomicBool::new(false),
     })
   }
 
-  pub fn pin(&self) {
+  fn pin(&self) {
     self.pin.fetch_add(1, Ordering::Release);
   }
-  pub fn unpin(&self) {
+  fn unpin(&self) {
     self.pin.fetch_sub(1, Ordering::Release);
+  }
+
+  pub fn is_pinned(&self) -> bool {
+    self.pin.load(Ordering::Acquire) > 0
   }
 
   pub fn replay(&self) -> Result {
@@ -56,8 +65,44 @@ impl TableHandle {
     &self.free_list
   }
 
+  pub fn closed(&self) -> bool {
+    self.closed.load(Ordering::Acquire)
+  }
+
   pub fn truncate(&self) -> Result {
+    if self.closed.fetch_or(true, Ordering::Release) {
+      return Ok(());
+    }
     self.disk.close();
     std::fs::remove_file(self.metadata.get_path()).map_err(Error::IO)
+  }
+}
+
+pub struct PinnedHandle(Arc<TableHandle>);
+impl PinnedHandle {
+  #[inline]
+  pub fn new(handle: Arc<TableHandle>) -> Self {
+    handle.pin();
+    Self(handle)
+  }
+
+  #[inline]
+  pub fn handle(&self) -> Arc<TableHandle> {
+    self.0.clone()
+  }
+}
+impl Drop for PinnedHandle {
+  #[inline]
+  fn drop(&mut self) {
+    self.0.unpin();
+  }
+}
+
+impl Deref for PinnedHandle {
+  type Target = TableHandle;
+
+  #[inline]
+  fn deref(&self) -> &Self::Target {
+    &self.0
   }
 }
