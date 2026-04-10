7554920b428cfaa2bda0a843ee0d4f0b81a7a457
qwp0905 <qwp0905@gmail.com>
2026-04-10 18:01:06 +0900

    remove closed flag and merging to pin

--- src/table/handle.rs
+++ src/table/handle.rs
@@ -1,10 +1,12 @@
 use std::{
   ops::Deref,
   sync::{
-    atomic::{AtomicBool, AtomicUsize, Ordering},
+    atomic::{AtomicUsize, Ordering},
     Arc,
   },
 };
+
+use crossbeam::utils::Backoff;
 
 use super::TableMetadata;
 use crate::{
@@ -13,12 +15,13 @@ use crate::{
   Error, Result,
 };
 
+const CLOSED: usize = 1 << (usize::BITS - 1);
+
 pub struct TableHandle {
   metadata: TableMetadata,
   disk: DiskController<PAGE_SIZE>,
   free_list: FreeList,
   pin: AtomicUsize,
-  closed: AtomicBool,
 }
 impl TableHandle {
   pub fn open(
@@ -32,7 +35,6 @@ impl TableHandle {
       disk,
       free_list: FreeList::new(),
       pin: AtomicUsize::new(0),
-      closed: AtomicBool::new(false),
     })
   }
 
@@ -43,8 +45,24 @@ impl TableHandle {
     self.pin.fetch_sub(1, Ordering::Release);
   }
 
-  pub fn is_pinned(&self) -> bool {
-    self.pin.load(Ordering::Acquire) > 0
+  pub fn try_pin(self: Arc<Self>) -> Option<PinnedHandle> {
+    let backoff = Backoff::new();
+    loop {
+      let c = self.pin.load(Ordering::Acquire);
+      if c & CLOSED != 0 {
+        return None;
+      }
+
+      if self
+        .pin
+        .compare_exchange(c, c + 1, Ordering::Release, Ordering::Acquire)
+        .is_ok()
+      {
+        return Some(PinnedHandle(Arc::clone(&self)));
+      }
+
+      backoff.spin();
+    }
   }
 
   pub fn replay(&self) -> Result {
@@ -66,13 +84,17 @@ impl TableHandle {
   }
 
   pub fn closed(&self) -> bool {
-    self.closed.load(Ordering::Acquire)
+    self.pin.load(Ordering::Acquire) & CLOSED != 0
+  }
+
+  pub fn try_close(&self) -> bool {
+    self
+      .pin
+      .compare_exchange(0, CLOSED, Ordering::Release, Ordering::Acquire)
+      .is_ok()
   }
 
   pub fn truncate(&self) -> Result {
-    if self.closed.fetch_or(true, Ordering::Release) {
-      return Ok(());
-    }
     self.disk.close();
     std::fs::remove_file(self.metadata.get_path()).map_err(Error::IO)
   }
