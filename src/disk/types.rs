f75fe8193d9715a70eda72e458e78bb77f171b97
qwp0905 <qwp0905@gmail.com>
2026-04-11 17:05:00 +0900

    add types byte size

--- src/disk/types.rs
+++ src/disk/types.rs
@@ -1,1 +1,2 @@
 pub type Pointer = u64;
+pub const POINTER_BYTES: usize = Pointer::BITS as usize >> 3;
--- src/wal/types.rs
+++ src/wal/types.rs
@@ -1,4 +1,6 @@
 pub type LogId = u64;
+pub const LOG_ID_BYTES: usize = LogId::BITS as usize >> 3;
 pub type TxId = u64;
+pub const TX_ID_BYTES: usize = TxId::BITS as usize >> 3;
 
 pub type SegmentGeneration = u64;
