15bb391b526af781326e89c441be148de934e5b9
qwp0905 <qwp0905@gmail.com>
2026-04-13 10:45:13 +0900

    add spin wait trait for mutex/rwlock

--- src/utils/lock.rs
+++ src/utils/lock.rs
@@ -1,5 +1,13 @@
 use std::sync::{Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
+const SPIN_LIMIT: usize = 7;
+
+fn spin(count: usize) {
+  for _ in 0..(1 << count) {
+    std::hint::spin_loop();
+  }
+}
+
 pub trait ShortenedMutex<T: ?Sized> {
   fn l(&self) -> MutexGuard<'_, T>;
 }
@@ -10,17 +18,58 @@ impl<T: ?Sized> ShortenedMutex<T> for Mutex<T> {
   }
 }
 
-pub trait ShortenedRwLock<T: ?Sized> {
-  fn rl(&self) -> RwLockReadGuard<'_, T>;
-  fn wl(&self) -> RwLockWriteGuard<'_, T>;
+pub trait SpinningWait<T: ?Sized> {
+  fn spin_lock(&self) -> MutexGuard<'_, T>;
 }
-impl<T: ?Sized> ShortenedRwLock<T> for RwLock<T> {
-  #[inline(always)]
-  fn rl(&self) -> RwLockReadGuard<'_, T> {
+impl<T: ?Sized> SpinningWait<T> for Mutex<T> {
+  #[inline]
+  fn spin_lock(&self) -> MutexGuard<'_, T> {
+    let mut backoff = 0;
+
+    while backoff < SPIN_LIMIT {
+      if let Ok(guard) = self.try_lock() {
+        return guard;
+      }
+      spin(backoff);
+      backoff += 1;
+    }
+
+    self.lock().unwrap()
+  }
+}
+
+pub trait SpinningRwWait<T: ?Sized> {
+  fn spin_rl(&self) -> RwLockReadGuard<'_, T>;
+  fn spin_wl(&self) -> RwLockWriteGuard<'_, T>;
+}
+impl<T: ?Sized> SpinningRwWait<T> for RwLock<T> {
+  #[inline]
+  fn spin_rl(&self) -> RwLockReadGuard<'_, T> {
+    let mut backoff = 0;
+
+    while backoff < SPIN_LIMIT {
+      if let Ok(guard) = self.try_read() {
+        return guard;
+      }
+      spin(backoff);
+      backoff += 1;
+    }
+
     self.read().unwrap()
   }
-  #[inline(always)]
-  fn wl(&self) -> RwLockWriteGuard<'_, T> {
+
+  #[inline]
+  fn spin_wl(&self) -> RwLockWriteGuard<'_, T> {
+    let mut backoff = 0;
+
+    while backoff < SPIN_LIMIT {
+      if let Ok(guard) = self.try_write() {
+        return guard;
+      }
+      spin(backoff);
+      backoff += 1;
+    }
+
     self.write().unwrap()
   }
 }
