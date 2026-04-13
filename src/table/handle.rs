3804afe599eb219d983510de7930c8db31cec1cd
qwp0905 <qwp0905@gmail.com>
2026-04-13 14:54:27 +0900

    fix borrow

--- src/table/handle.rs
+++ src/table/handle.rs
@@ -37,7 +37,7 @@ impl TableHandle {
     self.pin.fetch_sub(1, Ordering::Release);
   }
 
-  pub fn try_pin(self: Arc<Self>) -> Option<PinnedHandle> {
+  pub fn try_pin(self: &Arc<Self>) -> Option<PinnedHandle> {
     let backoff = Backoff::new();
     loop {
       let c = self.pin.load(Ordering::Acquire);
