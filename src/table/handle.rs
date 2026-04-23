44f1d3be1a88bd4fdd3825bac286d9f131ab8ee7
qwp0905 <qwp0905@gmail.com>
2026-04-23 16:02:16 +0900

    fix table pin to protect only mutation for table eg. gc, merge, compaction

--- src/table/handle.rs
+++ src/table/handle.rs
@@ -1,8 +1,8 @@
 use std::{
-  mem::forget,
+  mem::{forget, transmute},
   ops::Deref,
   sync::{
-    atomic::{AtomicU32, Ordering},
+    atomic::{AtomicBool, AtomicU32, Ordering},
     Arc,
   },
 };
@@ -10,25 +10,34 @@ use std::{
 use super::TableMetadata;
 use crate::{
   disk::{DiskController, FreeList, PAGE_SIZE},
-  utils::{ExclusivePin, SharedToken},
-  Error, Result,
+  utils::{ExclusivePin, ExclusiveToken, SharedToken},
+  Result,
 };
 
 pub struct TableHandle {
   metadata: TableMetadata,
   disk: DiskController<PAGE_SIZE>,
   free_list: FreeList,
+  /**
+   * pin for mutation (eg. compaction / merge nodes / gc)
+   */
   pin: ExclusivePin,
+  closed: AtomicBool,
   redirection_count: AtomicU32,
 }
 impl TableHandle {
-  pub fn new(metadata: TableMetadata, disk: DiskController<PAGE_SIZE>) -> Self {
+  pub fn new(metadata: &TableMetadata, disk: DiskController<PAGE_SIZE>) -> Self {
     Self {
-      metadata,
+      metadata: TableMetadata::new(
+        metadata.get_id(),
+        metadata.get_name().to_string(),
+        metadata.get_path().into(),
+      ),
       disk,
       free_list: FreeList::new(),
       pin: ExclusivePin::new(),
       redirection_count: AtomicU32::new(0),
+      closed: AtomicBool::new(false),
     }
   }
 
@@ -37,6 +46,15 @@ impl TableHandle {
     Some(PinnedHandle {
       handle: self,
       _token: token,
+    })
+  }
+
+  pub fn try_mutation(self: &Arc<Self>) -> Option<MutationHandle> {
+    let token = self.pin.try_exclusive()?;
+    Some(MutationHandle {
+      handle: self.clone(),
+      // transmute allowed since arc guarantees the lifespan
+      _token: unsafe { transmute(token) },
     })
   }
 
@@ -59,22 +77,27 @@ impl TableHandle {
     &self.free_list
   }
 
-  #[inline]
-  pub fn closed(&self) -> bool {
-    self.pin.is_exclusive()
-  }
-
   /**
    * Permanently and exclusively fix the table pin.
    * After calling this method, you cannot pin it forever.
    */
   #[inline]
   pub fn try_close(&self) -> bool {
-    self.pin.try_exclusive().map(forget).is_some()
+    if let Some(token) = self.pin.try_exclusive() {
+      forget(token);
+      self.closed.fetch_or(true, Ordering::Release);
+      return true;
+    }
+    false
   }
 
+  pub fn is_closed(&self) -> bool {
+    self.closed.load(Ordering::Acquire)
+  }
+
+  #[inline]
   pub fn truncate(&self) -> Result {
-    std::fs::remove_file(self.metadata.get_path()).map_err(Error::IO)
+    self.disk.truncate(self.metadata.get_path())
   }
 
   pub fn mark_redirection(&self) {
@@ -100,6 +123,30 @@ impl<'a> PinnedHandle<'a> {
 }
 
 impl<'a> Deref for PinnedHandle<'a> {
+  type Target = TableHandle;
+
+  #[inline]
+  fn deref(&self) -> &Self::Target {
+    &self.handle
+  }
+}
+
+pub struct MutationHandle {
+  handle: Arc<TableHandle>,
+  _token: ExclusiveToken<'static>,
+}
+impl MutationHandle {
+  #[inline]
+  pub fn handle(&self) -> &Arc<TableHandle> {
+    &self.handle
+  }
+
+  pub fn into_inner(self) -> Arc<TableHandle> {
+    self.handle
+  }
+}
+
+impl Deref for MutationHandle {
   type Target = TableHandle;
 
   #[inline]
