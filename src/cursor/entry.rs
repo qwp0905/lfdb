0b0d3bb53f310696968f4354a6c1025ed1633132
qwp0905 <qwp0905@gmail.com>
2026-04-28 16:48:28 +0900

    add data entry view for zero copy

--- src/cursor/entry.rs
+++ src/cursor/entry.rs
@@ -1,9 +1,10 @@
 use std::collections::VecDeque;
 
 use crate::{
-  disk::{Pointer, POINTER_BYTES},
+  disk::{Page, Pointer, POINTER_BYTES},
   serialize::{
-    Deserializable, Serializable, SerializeType, TypedObject, SERIALIZABLE_BYTES,
+    Deserializable, Serializable, SerializeType, TypedObject, Viewable,
+    SERIALIZABLE_BYTES,
   },
   wal::{TxId, TX_ID_BYTES},
   Error, Result,
@@ -33,29 +34,10 @@ pub enum RecordData {
 impl RecordData {
   pub const fn len(&self) -> usize {
     match self {
-      RecordData::Data(data) => 1 + 2 + data.len(),
-      RecordData::Chunked(pointers) => 1 + 1 + POINTER_BYTES * pointers.len(),
-      RecordData::Tombstone => 1,
+      Self::Data(data) => 1 + 2 + data.len(),
+      Self::Chunked(pointers) => 1 + 1 + POINTER_BYTES * pointers.len(),
+      Self::Tombstone => 1,
     }
-  }
-
-  pub fn read_data<F>(&self, read_chunk: F) -> Result<Option<Vec<u8>>>
-  where
-    F: Fn(Pointer) -> Result<DataChunk>,
-  {
-    let pointers = match self {
-      RecordData::Data(data) => return Ok(Some(data.clone())),
-      RecordData::Tombstone => return Ok(None),
-      RecordData::Chunked(p) => p,
-    };
-
-    let mut data = Vec::new();
-    for &ptr in pointers {
-      let chunk: DataChunk = read_chunk(ptr)?;
-      data.extend_from_slice(chunk.get_data());
-    }
-
-    Ok(Some(data))
   }
 }
 
@@ -121,11 +103,6 @@ impl DataEntry {
     self.versions = new_versions;
   }
 
-  #[allow(unused)]
-  pub fn get_versions(&self) -> impl Iterator<Item = &VersionRecord> {
-    self.versions.iter()
-  }
-
   pub fn get_last_owner(&self) -> Option<TxId> {
     self.versions.front().map(|v| v.owner)
   }
@@ -145,19 +122,6 @@ impl DataEntry {
     let byte_len =
       POINTER_BYTES + 2 + self.versions.iter().map(|v| v.byte_len()).sum::<usize>();
     record.byte_len() + byte_len <= SERIALIZABLE_BYTES
-  }
-
-  pub fn is_empty(&self) -> bool {
-    if self.versions.is_empty() {
-      return true;
-    }
-    if self.versions.len() > 1 {
-      return false;
-    }
-    if let RecordData::Tombstone = self.versions[0].data {
-      return true;
-    }
-    false
   }
 }
 impl TypedObject for DataEntry {
@@ -232,10 +196,6 @@ impl DataChunk {
   pub const fn new(chunk: Vec<u8>) -> Self {
     Self { chunk }
   }
-
-  pub fn get_data(&self) -> &[u8] {
-    &self.chunk
-  }
 }
 impl TypedObject for DataChunk {
   fn get_type() -> SerializeType {
@@ -254,6 +214,186 @@ impl Deserializable for DataChunk {
     let len = reader.read_u16()? as usize;
     let chunk = reader.read_n(len)?.to_vec();
     Ok(Self { chunk })
+  }
+}
+
+#[derive(Debug)]
+enum RecordDataView {
+  Data(usize, usize),
+  Chunked(Vec<Pointer>),
+  Tombstone,
+}
+#[derive(Debug)]
+struct VersionRecordView {
+  pub owner: TxId,
+  pub version: TxId,
+  pub data: RecordDataView,
+}
+impl VersionRecordView {
+  const fn new(owner: TxId, version: TxId, data: RecordDataView) -> Self {
+    Self {
+      owner,
+      version,
+      data,
+    }
+  }
+}
+
+pub struct DataEntryView<'a> {
+  page: &'a Page,
+  next: Option<Pointer>,
+  versions: Vec<VersionRecordView>,
+}
+impl<'a> TypedObject for DataEntryView<'a> {
+  fn get_type() -> SerializeType {
+    SerializeType::DataEntry
+  }
+}
+impl<'a> Viewable<'a> for DataEntryView<'a> {
+  fn read_from(
+    page: &'a Page,
+    reader: &mut crate::disk::PageScanner<'a>,
+  ) -> Result<Self> {
+    let next = reader.read_u64()?;
+    let len = reader.read_u16()? as usize;
+    let mut versions = Vec::with_capacity(len);
+    for _ in 0..len {
+      let version = reader.read_u64()?;
+      let owner = reader.read_u64()?;
+      let data = match reader.read()? {
+        0 => {
+          let l = reader.read_u16()? as usize;
+          let offset = reader.advance(l)?;
+          RecordDataView::Data(offset, offset + l)
+        }
+        1 => RecordDataView::Tombstone,
+        2 => {
+          let l = reader.read()? as usize;
+          let mut pointers = Vec::with_capacity(l);
+          for _ in 0..l {
+            pointers.push(reader.read_u64()?);
+          }
+          RecordDataView::Chunked(pointers)
+        }
+        _ => return Err(Error::InvalidFormat("invalid type for data version record")),
+      };
+      versions.push(VersionRecordView::new(owner, version, data));
+    }
+    Ok(Self {
+      page,
+      versions,
+      next: (next != 0).then_some(next),
+    })
+  }
+}
+impl<'a> Serializable for DataEntryView<'a> {
+  fn write_at(&self, writer: &mut crate::disk::PageWriter) -> Result {
+    writer.write_u64(self.next.unwrap_or(0))?;
+    writer.write_u16(self.versions.len() as u16)?;
+
+    for record in &self.versions {
+      writer.write_u64(record.version)?;
+      writer.write_u64(record.owner)?;
+      match &record.data {
+        RecordDataView::Data(s, e) => {
+          writer.write(&[0])?;
+          writer.write_u16((e - s) as u16)?;
+          writer.write(self.page.range(*s..*e))?;
+        }
+        RecordDataView::Tombstone => writer.write(&[1])?,
+        RecordDataView::Chunked(pointers) => {
+          writer.write(&[2])?;
+          writer.write_u8(pointers.len() as u8)?;
+          for ptr in pointers {
+            writer.write_u64(*ptr)?;
+          }
+        }
+      }
+    }
+    Ok(())
+  }
+}
+pub enum RecordDataRef<'a> {
+  Data(&'a [u8]),
+  Chunked(&'a [Pointer]),
+  Tombstone,
+}
+pub struct VersionRecordRef<'a> {
+  pub owner: TxId,
+  pub version: TxId,
+  pub data: RecordDataRef<'a>,
+}
+impl<'a> DataEntryView<'a> {
+  pub const fn get_next(&self) -> Option<Pointer> {
+    self.next
+  }
+
+  #[allow(unused)]
+  pub fn get_last_owner(&self) -> Option<TxId> {
+    self.versions.first().map(|v| v.owner)
+  }
+
+  pub fn chunked_pointers(&self) -> impl Iterator<Item = &[Pointer]> + '_ {
+    self.versions.iter().flat_map(|record| match &record.data {
+      RecordDataView::Data(_, _) => None,
+      RecordDataView::Chunked(pointers) => Some(pointers.as_slice()),
+      RecordDataView::Tombstone => None,
+    })
+  }
+
+  pub fn get_versions(&'a self) -> impl Iterator<Item = VersionRecordRef<'a>> + 'a {
+    self.versions.iter().map(|record| VersionRecordRef {
+      owner: record.owner,
+      version: record.version,
+      data: match &record.data {
+        RecordDataView::Data(s, e) => RecordDataRef::Data(self.page.range(*s..*e)),
+        RecordDataView::Chunked(pointers) => RecordDataRef::Chunked(pointers),
+        RecordDataView::Tombstone => RecordDataRef::Tombstone,
+      },
+    })
+  }
+
+  pub fn is_empty(&self) -> bool {
+    if self.versions.is_empty() {
+      return true;
+    }
+    if self.versions.len() > 1 {
+      return false;
+    }
+    if let RecordDataView::Tombstone = self.versions[0].data {
+      return true;
+    }
+    false
+  }
+}
+
+pub struct DataChunkView<'a> {
+  page: &'a Page,
+  start: usize,
+  end: usize,
+}
+impl<'a> TypedObject for DataChunkView<'a> {
+  fn get_type() -> SerializeType {
+    SerializeType::DataChunk
+  }
+}
+impl<'a> Viewable<'a> for DataChunkView<'a> {
+  fn read_from(
+    page: &'a Page,
+    scanner: &mut crate::disk::PageScanner<'a>,
+  ) -> Result<Self> {
+    let len = scanner.read_u16()? as usize;
+    let offset = scanner.advance(len)?;
+    Ok(Self {
+      page,
+      start: offset,
+      end: offset + len,
+    })
+  }
+}
+impl<'a> DataChunkView<'a> {
+  pub fn get_data(&self) -> &[u8] {
+    self.page.range(self.start..self.end)
   }
 }
 
