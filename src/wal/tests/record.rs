3a5c9f144eaed1ac8df36fa4dded737f7d5cb891
qwp0905 <qwp0905@gmail.com>
2026-04-29 16:45:04 +0900

    remove unused fields in checkpoint record

--- src/wal/tests/record.rs
+++ src/wal/tests/record.rs
@@ -52,22 +52,12 @@ fn test_insert_roundtrip() {
 fn test_checkpoint_roundtrip() {
   let log_id = 5;
   let last_log_id = 200;
-  let min_active = 123;
-  let current_version = 6;
   let path: PathBuf = format!("sdfsdf").into();
-  let r = LogRecord::new_checkpoint(
-    log_id,
-    last_log_id,
-    min_active,
-    current_version,
-    path.clone(),
-  );
+  let r = LogRecord::new_checkpoint(log_id, last_log_id, path.clone());
   let parsed = assert_roundtrip(&r);
   match parsed.operation {
-    Operation::Checkpoint(id, mc, v, p) => {
+    Operation::Checkpoint(id, p) => {
       assert_eq!(last_log_id, id);
-      assert_eq!(min_active, mc);
-      assert_eq!(current_version, v);
       assert_eq!(path, p);
     }
     _ => panic!("expected Checkpoint"),
@@ -82,7 +72,7 @@ fn test_entry_roundtrip() {
   let _ = writer.write(&(4 as u16).to_le_bytes());
   let r1 = LogRecord::new_start(1, 1);
   let r2 = LogRecord::new_insert(2, 1, 0, 10, vec![]);
-  let r4 = LogRecord::new_checkpoint(3, 123, 456, 1, format!("sdlfkj").into());
+  let r4 = LogRecord::new_checkpoint(3, 456, format!("sdlfkj").into());
   let r3 = LogRecord::new_commit(4, 1);
   let _ = writer.write(&r1.to_bytes_with_len());
   let _ = writer.write(&r2.to_bytes_with_len());
@@ -105,7 +95,7 @@ fn test_entry_roundtrip() {
   assert_eq!(d[2].log_id, 3);
   assert!(matches!(
     &d[2].operation,
-    Operation::Checkpoint(123, 456, 1, s) if
+    Operation::Checkpoint(456, s) if
     *s == PathBuf::from("sdlfkj"),
   ));
 
