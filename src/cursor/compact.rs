52f135be61b6f38863e9e6fad6afd6bce4571cf4
qwp0905 <qwp0905@gmail.com>
2026-05-28 13:22:45 +0900

    fix typo

--- src/cursor/compact.rs
+++ src/cursor/compact.rs
@@ -234,7 +234,7 @@ impl<'a> WritablePolicy for CompactionWritePolicy<'a> {
   }
 }
 
-pub const COMPACTION_INTERVAL: Duration = Duration::from_secs(10);
+pub const COMPACTION_INTERVAL: Duration = Duration::from_secs(1);
 
 pub fn wait_compaction(
   tables: Arc<TableMapper>,
