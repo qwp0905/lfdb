97f0e2bbdfef1553d7d91dd78f7c92284c44ab5f
qwp0905 <qwp0905@gmail.com>
2026-06-10 22:18:11 +0900

    add wal failed error from io error

--- src/error.rs
+++ src/error.rs
@@ -64,6 +64,9 @@ pub enum Error {
   #[error("wal unavailable to write. please drop engine and restart.")]
   WALUnavailable,
 
+  #[error("wal failed since {0}. please drop engine and restart.")]
+  WALFailed(io::ErrorKind),
+
   #[error("thread panic: {0:?}")]
   Panic(Arc<dyn Any + Send>),
 
@@ -103,6 +106,7 @@ impl Clone for Error {
       Self::KeyExceeded(e, r) => Self::KeyExceeded(*e, *r),
       Self::ValueExceeded(e, r) => Self::ValueExceeded(*e, *r),
       Self::WALUnavailable => Self::WALUnavailable,
+      Self::WALFailed(kind) => Self::WALFailed(*kind),
       Self::Panic(err) => Self::Panic(err.clone()),
     }
   }
