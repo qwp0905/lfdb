0ded29dfc5ffb3b6a127828d7ae3d854d6b101ce
qwp0905 <qwp0905@gmail.com>
2026-06-10 18:30:00 +0900

    add event bus test

--- src/background/tests/event_bus.rs
+++ src/background/tests/event_bus.rs
@@ -47,3 +47,41 @@ fn test_simple() {
   assert_eq!(E1_C.load(Ordering::Acquire), e1c);
   assert_eq!(E2_C.load(Ordering::Acquire), e2c);
 }
+
+#[test]
+fn test_shared() {
+  static E1_C: AtomicUsize = AtomicUsize::new(0);
+  static E2_C: AtomicUsize = AtomicUsize::new(0);
+
+  struct EV;
+  struct S1;
+  impl SharedSubscription<EV> for S1 {
+    fn handle(&self, _: Arc<EV>) {
+      E1_C.fetch_add(1, Ordering::Relaxed);
+    }
+  }
+  binding_events!(S1 { shared: [EV] });
+
+  struct S2;
+  impl SharedSubscription<EV> for S2 {
+    fn handle(&self, _: Arc<EV>) {
+      E2_C.fetch_add(1, Ordering::Relaxed);
+    }
+  }
+  binding_events!(S2 { shared: [EV] });
+
+  let s1 = Arc::new(S1);
+  let s2 = Arc::new(S2);
+
+  let ev = EventBus::new();
+  ev.register(&s1);
+  ev.register(&s2);
+
+  ev.publish(EV);
+  ev.publish(EV);
+  ev.publish(EV);
+  ev.close();
+
+  assert_eq!(E1_C.load(Ordering::Relaxed), 3);
+  assert_eq!(E2_C.load(Ordering::Relaxed), 3);
+}
