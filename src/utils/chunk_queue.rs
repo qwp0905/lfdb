5494dd2564c38de89ed72998f242023cc34db93a
qwp0905 <qwp0905@gmail.com>
2026-06-11 21:53:18 +0900

    add chunk queue

---
+++ src/utils/chunk_queue.rs
@@ -0,0 +1,108 @@
+use std::{mem::MaybeUninit, ptr::null_mut};
+
+const CAP: usize = 31;
+
+struct Block<T> {
+  next: *mut Block<T>,
+  data: [MaybeUninit<T>; CAP],
+  head: usize,
+  tail: usize,
+}
+impl<T> Block<T> {
+  const fn push(&mut self, value: T) {
+    self.data[self.tail].write(value);
+    self.tail += 1;
+  }
+  const fn new(value: T) -> Self {
+    let mut data = [const { MaybeUninit::uninit() }; CAP];
+    data[0].write(value);
+    Self {
+      next: null_mut(),
+      data,
+      head: 0,
+      tail: 1,
+    }
+  }
+  const fn pop(&mut self) -> T {
+    let value = unsafe { self.data[self.head].assume_init_read() };
+    self.head += 1;
+    value
+  }
+  const fn is_full(&self) -> bool {
+    self.tail == CAP
+  }
+  const fn is_empty(&self) -> bool {
+    self.head == self.tail
+  }
+}
+impl<T> Drop for Block<T> {
+  fn drop(&mut self) {
+    for i in self.head..self.tail {
+      unsafe { self.data[i].assume_init_drop() };
+    }
+  }
+}
+
+pub struct ChunkQueue<T> {
+  head: *mut Block<T>,
+  tail: *mut Block<T>,
+  len: usize,
+}
+impl<T> ChunkQueue<T> {
+  pub const fn new() -> Self {
+    Self {
+      head: null_mut(),
+      tail: null_mut(),
+      len: 0,
+    }
+  }
+  #[allow(unused)]
+  pub const fn len(&self) -> usize {
+    self.len
+  }
+  #[allow(unused)]
+  pub const fn is_empty(&self) -> bool {
+    self.len == 0
+  }
+  pub fn push(&mut self, value: T) {
+    self.len += 1;
+    if self.head.is_null() {
+      self.tail = Box::into_raw(Box::new(Block::new(value)));
+      self.head = self.tail;
+      return;
+    }
+
+    let tail = unsafe { &mut *self.tail };
+    if !tail.is_full() {
+      return tail.push(value);
+    };
+
+    tail.next = Box::into_raw(Box::new(Block::new(value)));
+    self.tail = tail.next;
+  }
+  pub fn pop(&mut self) -> Option<T> {
+    while !self.head.is_null() {
+      let head = unsafe { &mut *self.head };
+      if !head.is_empty() {
+        self.len -= 1;
+        return Some(head.pop());
+      }
+      self.head = unsafe { Box::from_raw(self.head) }.next;
+    }
+    None
+  }
+}
+impl<T> Drop for ChunkQueue<T> {
+  fn drop(&mut self) {
+    let mut ptr = self.head;
+    while !ptr.is_null() {
+      ptr = unsafe { Box::from_raw(ptr) }.next;
+    }
+  }
+}
+unsafe impl<T: Send> Send for ChunkQueue<T> {}
+unsafe impl<T: Sync> Sync for ChunkQueue<T> {}
+
+#[cfg(test)]
+#[path = "tests/chunk_queue.rs"]
+mod tests;
---
+++ src/utils/tests/chunk_queue.rs
@@ -0,0 +1,55 @@
+use std::{
+  mem::forget,
+  sync::atomic::{AtomicUsize, Ordering},
+};
+
+use super::*;
+
+#[test]
+fn test_push_and_pop() {
+  let mut queue = ChunkQueue::new();
+  let count = 1024;
+  for i in 0..count {
+    assert_eq!(queue.len(), i);
+    queue.push(i);
+  }
+
+  assert_eq!(queue.len(), count);
+
+  for i in 0..count {
+    assert_eq!(queue.len(), count - i);
+    assert_eq!(queue.pop(), Some(i));
+  }
+
+  assert!(queue.is_empty());
+  assert_eq!(queue.pop(), None);
+  assert_eq!(queue.pop(), None);
+  assert_eq!(queue.pop(), None);
+}
+
+#[test]
+fn test_drop() {
+  static COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+  struct DC;
+  impl Drop for DC {
+    fn drop(&mut self) {
+      COUNTER.fetch_add(1, Ordering::Relaxed);
+    }
+  }
+
+  let push_count = 65536;
+  let pop_count = 1297;
+  {
+    let mut queue = ChunkQueue::new();
+    for _ in 0..push_count {
+      queue.push(DC);
+    }
+
+    for _ in 0..pop_count {
+      queue.pop().map(forget);
+    }
+  }
+
+  assert_eq!(COUNTER.load(Ordering::Relaxed), push_count - pop_count);
+}
