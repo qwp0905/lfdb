ab7768c519bc96148afba1eba7e5031c0383e3b0
qwp0905 <qwp0905@gmail.com>
2026-06-13 22:01:36 +0900

    remove unused errors

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,7 @@
 use std::{
-  any::Any,
-  error, io,
+  io,
   panic::{RefUnwindSafe, UnwindSafe},
   result,
-  sync::Arc,
 };
 
 use thiserror::Error;
@@ -31,9 +29,6 @@ pub enum Error {
   #[error("engine unavailable")]
   EngineUnavailable,
 
-  #[error("worker closed")]
-  WorkerClosed,
-
   #[error("flush failed")]
   FlushFailed,
 
@@ -42,9 +37,6 @@ pub enum Error {
 
   #[error("thread conflict detected")]
   ThreadConflict,
-
-  #[error("channel disconnected")]
-  ChannelDisconnected,
 
   #[error("exceeded maximum table name length. maximum {0}, received {1}")]
   TableNameExceeded(usize, usize),
@@ -69,51 +61,6 @@ pub enum Error {
 
   #[error("failed to open base dir.")]
   DirOpenFailed,
-
-  #[error("thread panic: {0:?}")]
-  Panic(Arc<dyn Any + Send>),
-
-  #[error("unknown {0:?}")]
-  Unknown(String),
-}
-impl Error {
-  pub fn unknown<E>(err: E) -> Self
-  where
-    E: error::Error + Send + Sync + 'static,
-  {
-    Self::Unknown(err.to_string())
-  }
-  pub fn panic(err: impl Any + Send) -> Self {
-    Self::Panic(Arc::from(err))
-  }
-}
-impl Clone for Error {
-  fn clone(&self) -> Self {
-    match self {
-      Self::TableNotFound(str) => Self::TableNotFound(str.clone()),
-      Self::InvalidFormat(err) => Self::InvalidFormat(err),
-      Self::DeserializeError(e, r) => Self::DeserializeError(*e, *r),
-      Self::Unknown(err) => Self::Unknown(err.clone()),
-      Self::IO(err) => Self::IO(io::Error::new(err.kind(), err.to_string())),
-      Self::EOF => Self::EOF,
-      Self::TransactionClosed => Self::TransactionClosed,
-      Self::EngineUnavailable => Self::EngineUnavailable,
-      Self::WorkerClosed => Self::WorkerClosed,
-      Self::FlushFailed => Self::FlushFailed,
-      Self::WriteConflict => Self::WriteConflict,
-      Self::ThreadConflict => Self::ThreadConflict,
-      Self::ChannelDisconnected => Self::ChannelDisconnected,
-      Self::TableNameExceeded(e, r) => Self::TableNameExceeded(*e, *r),
-      Self::TableNameEmpty => Self::TableNameEmpty,
-      Self::NotAllowedChar(char) => Self::NotAllowedChar(*char),
-      Self::KeyExceeded(e, r) => Self::KeyExceeded(*e, *r),
-      Self::ValueExceeded(e, r) => Self::ValueExceeded(*e, *r),
-      Self::WALUnavailable => Self::WALUnavailable,
-      Self::DirOpenFailed => Self::DirOpenFailed,
-      Self::WALFailed(kind) => Self::WALFailed(*kind),
-      Self::Panic(err) => Self::Panic(err.clone()),
-    }
-  }
 }
 
 pub type Result<T = ()> = result::Result<T, Error>;
