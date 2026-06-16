8308956d944647510ebbbda1a0846ad6fc7e1f40
qwp0905 <qwp0905@gmail.com>
2026-06-16 12:22:15 +0900

    remove fullscan recovery in bootstrap

--- src/cursor/recovery.rs
+++ src/cursor/recovery.rs
@@ -1,17 +1,9 @@
-use std::{collections::HashSet, ops::Bound, sync::Arc};
+use std::ops::Bound;
 
-use crossbeam::queue::SegQueue;
-
-use super::{
-  BTreeIndex, BTreeNodeView, DataEntryView, MergeSortable, ReadonlyPolicy,
-  RecordDataView, TreeHeader, WritablePolicy, HEADER_POINTER,
-};
+use super::{BTreeIndex, MergeSortable, ReadonlyPolicy, WritablePolicy};
 use crate::{
-  background::once,
   cache::BlockCache,
-  debug,
   disk::Pointer,
-  info,
   table::{PinnedHandle, TableHandleRef, TableMapper, TableMetadata},
   transaction::{PageRecorder, VersionVisibility},
   wal::{TxId, RESERVED_TX},
@@ -117,89 +109,4 @@ pub fn open_tables(
   }
 
   Ok((handles, compactions))
-}
-
-pub fn recovery(block_cache: Arc<BlockCache>, tables: &TableMapper) -> Result {
-  let open_handles = Arc::new(SegQueue::new());
-  tables
-    .get_all()
-    .into_iter()
-    .for_each(|v| open_handles.push(v));
-
-  let threads = (0..5)
-    .map(|_| {
-      let block_cache = block_cache.clone();
-      let open_handles = open_handles.clone();
-      once(move || {
-        while let Some(table) = open_handles.pop() {
-          debug!(
-            "table {} start to collect orphaned blocks.",
-            table.get_name(),
-          );
-          release_orphaned(&block_cache, &table)?;
-        }
-        Ok(())
-      })
-    })
-    .collect::<Vec<_>>();
-
-  threads.into_iter().try_for_each(|th| th.wait())?;
-  info!("orphaned block has released successfully.");
-  Ok(())
-}
-
-fn release_orphaned(block_cache: &BlockCache, table: &TableHandleRef) -> Result {
-  let mut visited = HashSet::<Pointer>::from_iter([HEADER_POINTER]);
-  let root = block_cache
-    .read(HEADER_POINTER, table)?
-    .for_read()
-    .as_ref()
-    .deserialize::<TreeHeader>()?
-    .get_root();
-  let mut node_stack = vec![root];
-  let mut entry_stack = vec![];
-
-  while let Some(ptr) = node_stack.pop() {
-    visited.insert(ptr);
-    match block_cache
-      .read(ptr, table)?
-      .for_read()
-      .as_ref()
-      .view::<BTreeNodeView>()?
-    {
-      BTreeNodeView::Internal(node) => node_stack.extend(node.get_all_child()?),
-      BTreeNodeView::Leaf(node) => {
-        let mut iter = node.get_entries();
-        while let Some((_, _, record, ptr)) = iter.try_next()? {
-          entry_stack.push(ptr);
-          if let RecordDataView::Chunked(pointers) = &record.data {
-            visited.extend(pointers);
-          }
-        }
-      }
-    };
-  }
-
-  while let Some(ptr) = entry_stack.pop() {
-    visited.insert(ptr);
-    let slot = block_cache.read(ptr, table)?.for_read();
-    let entry: DataEntryView = slot.as_ref().view()?;
-    let mut iter = entry.get_versions();
-    while let Some(record) = iter.try_next()? {
-      if let RecordDataView::Chunked(pointers) = &record.data {
-        visited.extend(pointers);
-      }
-    }
-    if let Some(i) = entry.get_next() {
-      entry_stack.push(i)
-    }
-  }
-
-  let file_end = table.disk().len()?;
-
-  (0..file_end)
-    .filter(|i| !visited.remove(i))
-    .for_each(|i| table.free().dealloc(i));
-
-  Ok(())
 }
