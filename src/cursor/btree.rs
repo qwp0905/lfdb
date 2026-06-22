6f668cf9d0a300011fd4784a79cc274ad2a51a35
qwp0905 <qwp0905@gmail.com>
2026-06-22 11:54:59 +0900

    fix typos

--- src/cursor/btree.rs
+++ src/cursor/btree.rs
@@ -37,10 +37,10 @@ impl<Policy: ReadonlyPolicy> BTreeIndex<Policy> {
         BTreeNodeView::Internal(node) => ptr = node.find(key)?.unwrap_or_else(|i| i),
         BTreeNodeView::Leaf(node) => match node.find(key)? {
           NodeFindResult::NotFound(_) => return Ok(None),
-          NodeFindResult::Move(i) => ptr = i,
-          NodeFindResult::Found(_, record, i) => {
+          NodeFindResult::Move(next) => ptr = next,
+          NodeFindResult::Found(_, record, entry_ptr) => {
             if !self.0.is_visible(record.owner, record.version) {
-              break ptr = i;
+              break ptr = entry_ptr;
             }
             return Ok(Some(match &record.data {
               RecordDataView::Data(s, e) => Some(VecRef::refed(slot.page(), *s, *e)),
@@ -97,10 +97,10 @@ impl<Policy: ReadonlyPolicy> BTreeIndex<Policy> {
         BTreeNodeView::Internal(node) => ptr = node.find(key)?.unwrap_or_else(|i| i),
         BTreeNodeView::Leaf(node) => match node.find(key)? {
           NodeFindResult::NotFound(_) => return Ok(false),
-          NodeFindResult::Move(i) => ptr = i,
-          NodeFindResult::Found(_, record, i) => {
+          NodeFindResult::Move(next) => ptr = next,
+          NodeFindResult::Found(_, record, entry_ptr) => {
             if !self.0.is_visible(record.owner, record.version) {
-              break ptr = i;
+              break ptr = entry_ptr;
             }
             return Ok(!record.data.is_tombstone());
           }
@@ -380,8 +380,8 @@ impl<Policy: WritablePolicy> BTreeIndex<Policy> {
       self.0.fetch_slot(ptr, table)?.for_batch().mutate(|slot| {
         let leaf = slot.as_ref().view::<BTreeNodeView>()?.into_leaf()?;
         let mut node = match leaf.find(&key)? {
-          NodeFindResult::Move(i) => return Ok(ptr = i),
-          NodeFindResult::Found(i, old, entry_ptr) => {
+          NodeFindResult::Move(next) => return Ok(ptr = next),
+          NodeFindResult::Found(pos, old, entry_ptr) => {
             let is_aborted = self.0.is_aborted(old.owner);
             if old.version > snapshot.version && !is_aborted {
               return Ok(result = Some(Err(entry_ptr)));
@@ -389,17 +389,17 @@ impl<Policy: WritablePolicy> BTreeIndex<Policy> {
 
             let mut node = leaf.writable()?;
             let new_record = record.take().unwrap();
-            let old = node.replace_at(i, new_record);
+            let old = node.replace_at(pos, new_record);
             if !is_aborted {
               self.append_version_chain(entry_ptr, old, table)?;
             }
             node
           }
-          NodeFindResult::NotFound(i) => {
+          NodeFindResult::NotFound(pos) => {
             let mut node = leaf.writable()?;
             let entry_ptr = self.0.alloc_and_log(&DataEntry::empty(), table)?;
             let new_record = record.take().unwrap();
-            node.insert_at(i, key.to_vec(), new_record, entry_ptr);
+            node.insert_at(pos, key.to_vec(), new_record, entry_ptr);
             node
           }
         };
@@ -447,8 +447,8 @@ where
       self.0.fetch_slot(ptr, table)?.for_batch().mutate(|slot| {
         let leaf = slot.as_ref().view::<BTreeNodeView>()?.into_leaf()?;
         let (mut node, mut result) = match leaf.find(key)? {
-          NodeFindResult::Move(i) => return Ok(ptr = i),
-          NodeFindResult::Found(i, old, entry_ptr) => {
+          NodeFindResult::Move(pos) => return Ok(ptr = pos),
+          NodeFindResult::Found(pos, old, entry_ptr) => {
             if self.0.is_conflict(old.owner, old.version) {
               return Ok(state = LoopState::Conflict(old.owner));
             }
@@ -462,13 +462,13 @@ where
               self.0.current_version(),
               record,
             );
-            let old = node.replace_at(i, new_record);
+            let old = node.replace_at(pos, new_record);
             if !self.0.is_aborted(old.owner) && !self.0.is_owned(old.owner) {
               self.append_version_chain(entry_ptr, old, table)?;
             }
             (node, WriteResult::updated(false))
           }
-          NodeFindResult::NotFound(i) => {
+          NodeFindResult::NotFound(pos) => {
             if !create {
               return Ok(state = LoopState::Break(WriteResult::not_matched()));
             }
@@ -484,7 +484,7 @@ where
               self.0.current_version(),
               record,
             );
-            node.insert_at(i, key.to_vec(), new_record, entry_ptr);
+            node.insert_at(pos, key.to_vec(), new_record, entry_ptr);
             (node, WriteResult::inserted(false))
           }
         };
