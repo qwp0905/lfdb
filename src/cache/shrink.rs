98abdb9b57c2e092c43434e6d62e915a1a8a2eaa
qwp0905 <qwp0905@gmail.com>
2026-07-08 09:30:42 +0900

    remove shrink table deref

--- src/cache/shrink.rs
+++ src/cache/shrink.rs
@@ -1,16 +1,42 @@
 use std::{
-  collections::{vec_deque::Drain, VecDeque},
+  collections::{vec_deque, VecDeque},
   hash::{BuildHasher, Hash, RandomState},
   mem::replace,
-  ops::{Deref, DerefMut, RangeBounds},
+  ops::RangeBounds,
 };
 
-use hashbrown::{hash_table::Entry, Equivalent, HashTable};
+use hashbrown::{hash_table, Equivalent, HashTable};
 
 pub struct ShrinkTable<T>(HashTable<T>);
 impl<T> ShrinkTable<T> {
   pub const fn new() -> Self {
     Self(HashTable::new())
+  }
+
+  pub fn find(&self, hash: u64, eq: impl FnMut(&T) -> bool) -> Option<&T> {
+    self.0.find(hash, eq)
+  }
+
+  pub fn find_entry(
+    &mut self,
+    hash: u64,
+    eq: impl FnMut(&T) -> bool,
+  ) -> std::result::Result<hash_table::OccupiedEntry<'_, T>, hash_table::AbsentEntry<'_, T>>
+  {
+    self.0.find_entry(hash, eq)
+  }
+
+  pub fn len(&self) -> usize {
+    self.0.len()
+  }
+
+  pub fn insert_unique(
+    &mut self,
+    hash: u64,
+    value: T,
+    hasher: impl Fn(&T) -> u64,
+  ) -> hash_table::OccupiedEntry<'_, T> {
+    self.0.insert_unique(hash, value, hasher)
   }
 
   pub fn remove_and_shrink(
@@ -36,17 +62,22 @@ impl<T> ShrinkTable<T> {
     self.0.shrink_to(cap >> 1, hasher);
     Some(v)
   }
-}
-impl<T> Deref for ShrinkTable<T> {
-  type Target = HashTable<T>;
 
-  fn deref(&self) -> &Self::Target {
-    &self.0
+  pub fn drain(&mut self) -> hash_table::Drain<'_, T> {
+    self.0.drain()
   }
-}
-impl<T> DerefMut for ShrinkTable<T> {
-  fn deref_mut(&mut self) -> &mut Self::Target {
-    &mut self.0
+
+  fn entry(
+    &mut self,
+    hash: u64,
+    eq: impl FnMut(&T) -> bool,
+    hasher: impl Fn(&T) -> u64,
+  ) -> hash_table::Entry<'_, T> {
+    self.0.entry(hash, eq, hasher)
+  }
+
+  fn iter(&self) -> hash_table::Iter<'_, T> {
+    self.0.iter()
   }
 }
 
@@ -74,14 +105,13 @@ impl<K> ShrinkSet<K> {
     let eq = Self::equivalent(key);
     self.0.find(hash, eq).is_some()
   }
-  pub fn insert<S>(&mut self, key: K, hash: u64, hasher: &S)
+  pub fn insert_unchecked<S>(&mut self, key: K, hash: u64, hasher: &S)
   where
-    K: Hash + Eq,
+    K: Hash,
     S: BuildHasher,
   {
-    let eq = Self::equivalent(&key);
     let hasher = Self::make_hasher(hasher);
-    self.0.entry(hash, eq, hasher).or_insert(key);
+    self.0.insert_unique(hash, key, hasher);
   }
   pub fn remove<Q, S>(&mut self, hash: u64, key: &Q, hasher: &S)
   where
@@ -128,11 +158,11 @@ impl<K, V> ShrinkMap<K, V> {
     let eq = Self::equivalent(&key);
     let hasher = Self::make_hasher(&self.hasher);
     match self.table.entry(hash, eq, hasher) {
-      Entry::Occupied(mut entry) => {
+      hash_table::Entry::Occupied(mut entry) => {
         let (_, old) = entry.get_mut();
         Some(replace(old, value))
       }
-      Entry::Vacant(entry) => {
+      hash_table::Entry::Vacant(entry) => {
         entry.insert((key, value));
         None
       }
@@ -189,7 +219,7 @@ impl<T> ShrinkQueue<T> {
     self.0.shrink_to(cap >> 1);
     Some(v)
   }
-  pub fn drain<R: RangeBounds<usize>>(&mut self, range: R) -> Drain<'_, T> {
+  pub fn drain<R: RangeBounds<usize>>(&mut self, range: R) -> vec_deque::Drain<'_, T> {
     self.0.drain(range)
   }
 }
