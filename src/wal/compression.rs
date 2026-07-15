ae38f3737180a0e77ac8682022654611bd13b2ac
qwp0905 <qwp0905@gmail.com>
2026-07-15 13:05:37 +0900

    add record encoding type to compress wal record

---
+++ src/wal/compression.rs
@@ -0,0 +1,52 @@
+pub struct DecompressError;
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy)]
+pub enum RecordEncoding {
+  Raw = 1u8,
+  Lz4 = 2u8,
+  Zstd = 3u8,
+}
+impl RecordEncoding {
+  pub fn from_byte(byte: u8) -> Option<Self> {
+    match byte {
+      1 => Some(Self::Raw),
+      2 => Some(Self::Lz4),
+      3 => Some(Self::Zstd),
+      _ => None,
+    }
+  }
+  pub fn compress(&self, data: &[u8]) -> Vec<u8> {
+    match self {
+      Self::Raw => {
+        let mut v = vec![0; data.len()];
+        v.copy_from_slice(data);
+        v
+      }
+      Self::Lz4 => lz4_flex::compress(data),
+      Self::Zstd => zstd::bulk::compress(data, 1).unwrap(),
+    }
+  }
+
+  pub fn decompress(
+    &self,
+    data: &[u8],
+    original_len: usize,
+  ) -> std::result::Result<Vec<u8>, DecompressError> {
+    let decoded = match self {
+      Self::Raw => {
+        let mut v = vec![0; data.len()];
+        v.copy_from_slice(data);
+        Ok(v)
+      }
+      Self::Lz4 => lz4_flex::decompress(data, original_len).map_err(|_| DecompressError),
+      Self::Zstd => {
+        zstd::bulk::decompress(data, original_len).map_err(|_| DecompressError)
+      }
+    }?;
+    if decoded.len() != original_len {
+      return Err(DecompressError);
+    }
+    Ok(decoded)
+  }
+}
