a1c9af0089b9133875ab12fc0ca085f3348d3c56
qwp0905 <qwp0905@gmail.com>
2026-08-17 10:23:28 +0900

    add compression to checkpoint snapshot file

--- src/transaction/snapshot.rs
+++ src/transaction/snapshot.rs
@@ -1,10 +1,12 @@
 use crate::{
   blob::BlobMetadata,
   disk::{AppendIOHandle, ScanIOHandle},
-  utils::{OffsetReader, OffsetWriter},
-  wal::{TxId, TX_ID_BYTES},
+  utils::{Encoding, OffsetReader, OffsetWriter, OpaqueRead, OpaqueWrite},
+  wal::TxId,
   Error, Result,
 };
+
+const DEFAULT_ENCODING: Encoding = Encoding::Lz4;
 
 pub struct CheckpointSnapshot {
   pub active_versions: Vec<TxId>,
@@ -32,76 +34,85 @@ impl CheckpointSnapshot {
   }
 
   pub fn read_from(file: &mut ScanIOHandle) -> Result<Self> {
-    let mut active_versions = Vec::new();
-    let mut aborted_versions = Vec::new();
-    let mut blob_metadata = Vec::new();
-
     {
-      let len = u32::from_le_bytes(file.read_to_vec(4)?.try_into().unwrap());
-      for _ in 0..len {
-        let id = TxId::from_le_bytes(file.read_to_vec(TX_ID_BYTES)?.try_into().unwrap());
-        active_versions.push(id);
-      }
-    }
+      let mut decoder = DEFAULT_ENCODING.decoder(file);
 
-    {
-      let len = u32::from_le_bytes(file.read_to_vec(4)?.try_into().unwrap());
-      for _ in 0..len {
-        let id = TxId::from_le_bytes(file.read_to_vec(TX_ID_BYTES)?.try_into().unwrap());
-        aborted_versions.push(id);
+      let mut active_versions = Vec::new();
+      let mut aborted_versions = Vec::new();
+      let mut blob_metadata = Vec::new();
+      {
+        let len = u32::from_le_bytes(decoder.read_array_opaque()?);
+        for _ in 0..len {
+          let id = TxId::from_le_bytes(decoder.read_array_opaque()?);
+          active_versions.push(id);
+        }
       }
-    }
 
-    {
-      let len = u32::from_le_bytes(file.read_to_vec(4)?.try_into().unwrap());
-      for _ in 0..len {
-        let byte_len =
-          u32::from_le_bytes(file.read_to_vec(4)?.try_into().unwrap()) as usize;
-        let mut bytes = vec![0; byte_len];
-        file.read(&mut bytes)?;
-        let Some(metadata) = BlobMetadata::read_from(&mut OffsetReader::new(&bytes))
-        else {
-          return Err(Error::InvalidFormat("blob metadata crashed."));
-        };
-        blob_metadata.push(metadata);
+      {
+        let len = u32::from_le_bytes(decoder.read_array_opaque()?);
+        for _ in 0..len {
+          let id = TxId::from_le_bytes(decoder.read_array_opaque()?);
+          aborted_versions.push(id);
+        }
       }
-    }
 
-    Ok(Self {
-      active_versions,
-      aborted_versions,
-      blob_metadata,
-    })
+      {
+        let len = u32::from_le_bytes(decoder.read_array_opaque()?);
+        for _ in 0..len {
+          let byte_len = u32::from_le_bytes(decoder.read_array_opaque()?) as usize;
+          let bytes = decoder.read_to_vec_opaque(byte_len)?;
+          let Some(metadata) = BlobMetadata::read_from(&mut OffsetReader::new(&bytes))
+          else {
+            return Err(Error::InvalidFormat("checkpoint snapshot crashed."));
+          };
+          blob_metadata.push(metadata);
+        }
+      }
+
+      Ok(Self {
+        active_versions,
+        aborted_versions,
+        blob_metadata,
+      })
+    }
   }
 
-  pub fn write_at(&self, file: &mut AppendIOHandle) -> Result {
+  fn write_encoded(&self, file: &mut AppendIOHandle) -> Result<()> {
+    let mut encoder = DEFAULT_ENCODING.encoder(file);
     {
-      file.append(&(self.active_versions.len() as u32).to_le_bytes())?;
+      encoder.write_opaque(&(self.active_versions.len() as u32).to_le_bytes())?;
       for id in self.active_versions.iter() {
-        file.append(&id.to_le_bytes())?;
+        encoder.write_opaque(&id.to_le_bytes())?;
       }
     }
 
     {
-      file.append(&(self.aborted_versions.len() as u32).to_le_bytes())?;
+      encoder.write_opaque(&(self.aborted_versions.len() as u32).to_le_bytes())?;
       for id in self.aborted_versions.iter() {
-        file.append(&id.to_le_bytes())?;
+        encoder.write_opaque(&id.to_le_bytes())?;
       }
     }
 
     {
-      file.append(&(self.blob_metadata.len() as u32).to_le_bytes())?;
+      encoder.write_opaque(&(self.blob_metadata.len() as u32).to_le_bytes())?;
       for metadata in self.blob_metadata.iter() {
         let byte_len = metadata.byte_len();
-        file.append(&(byte_len as u32).to_le_bytes())?;
+        encoder.write_opaque(&(byte_len as u32).to_le_bytes())?;
 
         let mut bytes = vec![0; byte_len];
         metadata.write_at(&mut OffsetWriter::new(&mut bytes));
-        file.append(&bytes)?;
+        encoder.write_opaque(&bytes)?;
       }
     }
+    encoder.flush_opaque()?;
 
-    file.flush()?;
+    Ok(())
+  }
+
+  pub fn write_at(&self, file: &mut AppendIOHandle) -> Result {
+    self.write_encoded(file)?;
+    file.flush_opaque()?;
+    file.sync()?;
     Ok(())
   }
 }
