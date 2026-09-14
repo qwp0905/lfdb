39ca3ee04d999d084eb0a2edb191b5e91cb5f591
qwp0905 <qwp0905@gmail.com>
2026-09-14 13:06:41 +0900

    fix typo

--- src/disk/types.rs
+++ src/disk/types.rs
@@ -1,3 +1,6 @@
+use std::sync::atomic::AtomicU64;
+
 /// Logical disk block pointer used by the storage layer.
 pub type Pointer = u64;
 pub const POINTER_BYTES: usize = Pointer::BITS as usize >> 3;
+pub type AtomicDiskPointer = AtomicU64;
