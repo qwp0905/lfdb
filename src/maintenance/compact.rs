f11cca172c21347ced151c02fbeb68a8047884c5
qwp0905 <qwp0905@gmail.com>
2026-09-23 14:04:14 +0900

    Revert "fix error in readable"
    This reverts commit 8f37392261f1ebadeeea7a4d7be154d66f32da6f.

--- src/maintenance/compact.rs
+++ src/maintenance/compact.rs
@@ -115,7 +115,7 @@ impl<'a> ReadonlyPolicy for MiniTx<'a> {
       .is_some_and(|s| s.get_id() == owner)
   }
   fn is_readable(&self, version: TxId) -> bool {
-    version < self.state.get_upper_bound()
+    version <= self.state.get_upper_bound()
   }
   fn is_active(&self, owner: TxId) -> bool {
     self.state.is_active(&owner)
